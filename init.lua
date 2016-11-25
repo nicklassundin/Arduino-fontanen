b2aa5b6ddad54e22eef47063d4bf31b82053de4d
hewge <hgglund.johan@gmail.com>
2016-11-25 14:19:53 +0100

    Added some helper functions for communicating with Adafruit.io.

--- init.lua
+++ init.lua
@@ -1,8 +1,36 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("fontanhuset", "3131313131")
-uart.on("data", "!",
-    function(data)
-        print("Här skall jag (serializera) data?:", data)
-        print("och skicka detta till en webserver via en POST req?")
-    end
-)
+function getip ()
+    print("Getting IP")
+    http.get('https://httpbin.org/ip', nil, function(code, data)
+        if (code < 0) then
+            print("HTTP request failed")
+        else
+            print(code, data)
+        end
+    end)
+end
+
+function getfeed ()
+    print("Getting feed")
+    http.get('http://io.adafruit.com/api/feeds/?X-AIO-Key=33980f0292044b3392a4a38a04bb4a7d', nil,  function(code, data)
+        if (code < 0) then
+            print("HTTP GET request failed")
+        else
+            print(code, data)
+        end
+    end)
+end
+
+function postdata (data)
+    print("Putting Data in feed", data)
+    http.post('http://io.adafruit.com/api/feeds/ESP/data?X-AIO-Key=33980f0292044b3392a4a38a04bb4a7d', 'Content-Type: application/x-www-form-urlencoded\r\n', 'value='..data, function(code, data)
+        if (code < 0) then
+            print("Http POST request failed")
+        else
+            print(code, data)
+        end
+    end)
+end
+uart.on("data", "!", postdata(data))
+
